b2fd8369c8dafb7643bf35b6c130d2982bdabf65
Jose Bezerra <jbqneto@gmail.com>
2021-03-03 23:44:08 -0300

    init nextjs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
-import '../styles/globals.css'
+import '../src/styles/global.css'
+import '../src/components/ExperienceBar/style.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
