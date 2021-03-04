427b8a30489a2fe7131816b2895e88c1d2f079e5
Jose Bezerra <jbqneto@gmail.com>
2021-03-04 08:44:06 -0300

    Aula 2 finalizada

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import { CompletedChallanges } from '../src/components/CompletedChallanges';
+import { Countdown } from '../src/components/Countdown';
 import ExperienceBar from '../src/components/ExperienceBar';
 import { Profile } from '../src/components/Profile';
 
@@ -8,12 +9,16 @@ import styles from '../src/styles/Home.module.css';
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <link rel="shortcut icon" href="favicon.png" type="image/png/" />
+      </Head>
       <ExperienceBar />
        
        <section>
          <div>
            <Profile />
            <CompletedChallanges />
+           <Countdown />
          </div>
          <div>
 
