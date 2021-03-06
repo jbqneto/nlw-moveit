8f6eba15280178e9902139c6d2be22dea78badb0
Jose Bezerra <jbqneto@gmail.com>
2021-03-06 10:45:58 -0300

    Aula 3 - Fim do ChallengeBox

--- src/components/ChallengeBox/index.tsx
+++ src/components/ChallengeBox/index.tsx
@@ -1,15 +1,41 @@
+import { useEffect, useState } from 'react';
 import styles from './ChallengeBox.module.css';
 
 export function ChallengeBox() {
+  
+  const [isChallengeActive, setChallengeActive] = useState(false);
+
+  useEffect(() => {
+    setChallengeActive(false);
+  }, [])
+
   return (
     <div className={styles.challengeBoxContainer}>
-      <div className={styles.challengeNotActive}>
-        <strong>Finalize um ciclo para receber um desafio</strong>
-        <p>
-          <img src="icons/level-up.svg" alt="Leveu up"/>
-          Avance de level completando desafios
-        </p>
+      { !isChallengeActive ? (
+      <div className={styles.challengeActive}>
+        <header>Ganhe 400 xp</header>
+
+        <main>
+          <img src="icons/body.svg" alt=""/>
+          <strong>Novo desafio</strong>
+          <p>Levante e faça alguma coisa</p>
+        </main>
+
+        <footer>
+          <button className={styles.challengeFailed} type="button">Falhei</button>
+          <button className={styles.challengeSucceded} type="button">Completei</button>
+        </footer>
+
       </div>
+      ) : (
+        <div className={styles.challengeNotActive}>
+          <strong>Finalize um ciclo para receber um desafio</strong>
+          <p>
+            <img src="icons/level-up.svg" alt="Leveu up"/>
+            Avance de level completando desafios
+          </p>
+        </div>
+      ) }
     </div>
   )
 }
